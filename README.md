2c694749681f658fb182c7d88e5f9b25e1e85e63
marcel <rieger@physik.rwth-aachen.de>
2014-10-07 10:41:54 +0200

    Initial commit, add existing files.

---
+++ README.md
@@ -0,0 +1,41 @@
+# CMSSW Lookup Sublime Text 2/3 Plugin
+
+This ST2/3 plugin lets you easily lookup python based CMSSW code from within your config files.
+
+
+## Usage
+
+1.  (Text-) Select an import path of a CMSSW module, i.e. the path passed to the ``process.load``
+    call or the module path of a real python ``import``.
+
+2.  Execute the _CMSSW Lookup_ command, e.g. via the command palette.
+
+3.  If a quick panel shows up, select the _lookup type_: you either may want to open a python file or a folder.
+    By default, your webbrowser will open the desired resource on [github.com](https://github.com/cms-sw/cmssw).
+
+![cmsswlookup usage](https://raw.githubusercontent.com/riga/CMSSWLookup/master/img/sh.png)
+
+With the default configuration, the example above opens
+https://github.com/cms-sw/cmssw/blob/CMSSW_5_3_19/RecoJets/JetProducers/python/ak5GenJets_cfi.py.
+
+
+## Configuration
+
+This plugin comes with a small number of package settings to guarantee proper results on your OS.
+
+  - ``branch``: the branch to want to lookup, defaults to ``"CMSSW_5_3_19"``
+  - ``url_format``: the url format you like to use, defaults to ``"https://github.com/cms-sw/cmssw/blob/%(branch)s/%(path)s%(postfix)s"``
+  - ``open_cms``: the command your OS uses to open websites
+  - ``extensions``: extensions to use, defaults to python files and folders (see above)
+
+
+## Development
+
+- Source hosted at [GitHub](https://github.com/riga/CMSSWLookup)
+- Report issues, questions, feature requests on
+[GitHub Issues](https://github.com/riga/CMSSWLookup/issues)
+
+
+## Authors
+
+Marcel R. ([riga](https://github.com/riga))
---
+++ cmsswlookup.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# python imports
+import os
+import re
+
+# sublime imports
+import sublime, sublime_plugin
+
+
+class CmsswLookupCommand(sublime_plugin.TextCommand):
+
+    def __init__(self, *args, **kwargs):
+        super(CmsswLookupCommand, self).__init__(*args, **kwargs)
+
+        self._settings = None
+
+    def run(self, edit):
+        # load settings
+        if self._settings is None:
+            self._settings = sublime.load_settings("CMSSWLookup.sublime-settings")
+
+        # load extensions (lists, 0 -> label, 1 -> postfix (e.g. ".py"))
+        extensions = self._settings.get("extensions")
+        if not len(extensions):
+            self.log("no extensions found")
+            return
+
+        labels    = [ext[0] for ext in extensions]
+        postfixes = [ext[1] for ext in extensions]
+
+        # lookup callback
+        def do_lookup(idx):
+            if ~idx:
+                self.lookup(self.view, postfixes[idx])
+
+        # > 1 extensions => show quick panel
+        if len(extensions) == 1:
+            do_lookup(0)
+        else:
+            self.view.window().show_quick_panel(labels, do_lookup)
+
+
+    def lookup(self, view, postfix):
+        for region in view.sel():
+            # the region must not be empty
+            if region.empty():
+                continue
+
+            # get the selected text
+            text = view.substr(region)
+
+            # convert the python-style import path to a url-style path
+            path = self.convert_path(text, postfix)
+
+            if not path:
+                self.log("no valid path to expand")
+            else:
+                # build and execute the command
+                cmd = " ".join([self._settings.get("open_cmd"), path])
+                self.log(cmd)
+                os.system(cmd)
+
+            # only use the first non-empty region
+            break
+
+
+    def convert_path(self, path, postfix):
+        # simple conversion, e.g. "some.python.file" => "some/python/file.py"
+        parts = path.strip().replace("/", ".").split(" ")[0].split(".")
+
+        # we lookup python files, the CMSSW folder structur yields a "python" folder
+        # after the second part (module.submodule."python".some.path)
+        parts.insert(2, "python")
+
+        # create data for formatting
+        data = {
+            "branch" : self._settings.get("branch"),
+            "path"   : "/".join(parts),
+            "postfix": postfix
+        }
+
+        return self._settings.get("url_format") % data
+
+
+    @staticmethod
+    def log(*msg):
+        print("CMSSW Lookup:", *msg)
